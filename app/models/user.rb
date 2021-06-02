f5d927083ad8b11292abeb3af76948db90937bfa
Massim Shah <massimshah@hotmail.com>
2021-06-02 14:31:11 +0900

    added dependent destroys and two methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_friendship
-  has_many :pets
-  has_many :petsittings
-  has_many :bookings
-  has_many :reviews
+  has_many :pets, dependent: :destroy
+  has_many :petsittings, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :bookings_as_owner, through: :petsittings, source: :bookings
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
+  before_destroy :destroy_messages
 
   def messages_with(friend)
     Message.where(sender: friend, receiver: self).or(Message.where(sender: self, receiver: friend))
+  end
+
+  def destroy_messages
+    messages.destroy_all
+  end
+
+  def messages
+    Message.where(receiver: self).or(Message.where(sender: self))
   end
 end
