b2ebb59b2fa286994a271fb8e77d449ada684bbb
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-06-02 11:42:28 +0900

    fixed controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,7 @@ class PetsController < ApplicationController
     authorize @pet
     @pet.user = @user
     if @pet.save
-      redirect_to pets_path
+      redirect_to user_path
     else
       render :new
     end
@@ -24,6 +24,6 @@ class PetsController < ApplicationController
   end
 
   def pet_params
-    params.require(:pet).permit(:pet_name, :birthdate, :species, :breed, :description, :user_id)
+    params.require(:pet).permit(:pet_name, :birthdate, :species, :breed, :description, :user_id, :photo)
   end
 end
