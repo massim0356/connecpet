4388c849c54b6db2f4c4474ee47264e83ab7bd0a
Massim Shah <massimshah@hotmail.com>
2021-06-03 11:44:26 +0900

    fixed subscribe for chat

--- app/channels/friend_channel.rb
+++ app/channels/friend_channel.rb
@@ -1,6 +1,8 @@
 class FriendChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    receiver = User.find(params[:id])
+    stream_for receiver
   end
 
   def unsubscribed
