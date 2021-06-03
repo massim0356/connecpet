0df143962af8f882eb6dce732920bce10fc75728
Massim Shah <massimshah@hotmail.com>
2021-06-03 19:44:37 +0900

    deleted p

--- app/channels/friend_channel.rb
+++ app/channels/friend_channel.rb
@@ -2,7 +2,6 @@ class FriendChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     receiver = User.find(params[:id])
-    p receiver
     stream_for receiver
   end
 
