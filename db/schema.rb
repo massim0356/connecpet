6b3436ed0ef3d85caac5dd29198528174d6b42b6
Massim Shah <massimshah@hotmail.com>
2021-06-07 17:56:21 +0900

    added read boolean to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_040256) do
+ActiveRecord::Schema.define(version: 2021_06_07_084833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_040256) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "sender_id", null: false
     t.bigint "receiver_id", null: false
+    t.boolean "read", default: false
     t.index ["receiver_id"], name: "index_messages_on_receiver_id"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
