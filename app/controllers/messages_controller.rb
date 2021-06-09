7bd59b6d31bd7a7614e80cc300cafaa6fffec089
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-06-09 17:53:33 +0900

    updated message time

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,16 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.sender = current_user
     @message.receiver = User.find(params[:user_id])
+    @messages = @message.sender.messages_with(@message.receiver).order(:created_at)
     authorize @message
     if @message.save
       FriendChannel.broadcast_to(
         @message.sender,
-        render_to_string(partial: 'messages/message', locals: { message: @message, viewer: @message.receiver })
+        render_to_string(partial: 'messages/message', locals: { message: @message, viewer: @message.receiver, messages: @messages })
       )
       FriendChannel.broadcast_to(
         @message.receiver,
-        render_to_string(partial: 'messages/message', locals: { message: @message, viewer: @message.sender })
+        render_to_string(partial: 'messages/message', locals: { message: @message, viewer: @message.sender, messages: @messages })
       )
       redirect_to friend_path(@message.receiver, anchor: "message-#{@message.id}")
     else
