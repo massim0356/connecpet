345809cc2e332660dcf3cd397f756087a72df07f
Massim Shah <massimshah@hotmail.com>
2021-06-09 21:34:20 +0900

    added destroy

--- app/models/pet_sitting.rb
+++ app/models/pet_sitting.rb
@@ -1,6 +1,6 @@
 class PetSitting < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   validates :user, uniqueness: true
 end
