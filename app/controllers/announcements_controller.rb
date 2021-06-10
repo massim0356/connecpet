d7a874c139f16dbaa4aa1c29089d5ecdf0b4daad
Machiko Takahashi <machiko.ihia22@gmail.com>
2021-06-10 11:52:05 +0900

    edited announcement controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,24 +1,21 @@
 class AnnouncementsController < ApplicationController
-  before_action :set_pet, only: [:new, :create]
+  before_action :set_user, only: [:new, :create]
 
   def index
     @announcements = policy_scope(Announcement)
     #change this to confirm the booking
-    @bookings = current_user.bookings
     # @announcement = policy_scope(Announcement).where(user: current_user)
     # @announcements_as_owner = policy_scope(Announcement).where(announcement: current_user.pets)
   end
 
   def new
     @announcement = Announcement.new
-    set_pet
     authorize @announcement
   end
 
   def create
     @announcement = Announcement.new(announcement_params)
     authorize @announcement
-    @announcement.pet = @pet
     @announcement.user = current_user
     if annoucement.save
       redirect_to annoucement_path
@@ -29,12 +26,11 @@ class AnnouncementsController < ApplicationController
 
   private
 
-  def set_pet
+  def set_user
     @user = current_user
-    @pet = @user.pets
   end
 
   def announcement_params
-    params.require(:annoucement).permit(:date, :content, :location, :status, :pet)
+    params.require(:annoucement).permit(:date, :content, :location, :title, :user_id)
   end
 end
